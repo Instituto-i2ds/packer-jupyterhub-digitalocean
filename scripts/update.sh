53f77f253111c1817319ba4ae631a02fa4f252fd
galvarado <guillermoalvarado89@gmail.com>
2020-12-01 16:44:15 -0600

    adding a sleep to solve the apt lock issue

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # Update
-sudo apt-get update -y
+sudo add-apt-repository universe -y && sudo apt-get update -y && sudo apt-get install build-essential libssl-dev libffi-dev python-dev software-properties-common python3-pip python3-venv -y 
+sudo pip3 install virtualenv 
