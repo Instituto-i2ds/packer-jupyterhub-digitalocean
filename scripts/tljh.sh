bb8181c44d4ffb3681f09a893555690aaa77760a
galvarado <guillermoalvarado89@gmail.com>
2021-01-26 10:15:23 -0600

    fixing typo

--- scripts/tljh.sh
+++ scripts/tljh.sh
@@ -2,6 +2,6 @@
 # Getting The Littlest JupyterHub, A simple JupyterHub distribution
 # for a small (0-100) number of users on a single server.
 # More details at: https://tljh.jupyter.org/
-sleep10
+sleep 10
 wget https://tljh.jupyter.org/bootstrap.py
 sudo python3 bootstrap.py --admin superadmin
